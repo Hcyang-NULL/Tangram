3e20a25178cd7703307dbfb452685af019d0faa1
haunt1 <1042335125@qq.com>
2019-05-31 16:27:24 +0800

    gen mid

--- code.py
+++ code.py
@@ -15,16 +15,47 @@ def Gensimple():
         else:
             if temp1 >= temp2:
                 temp3 = temp1-temp2
-                var+=str(temp1)+'-'+str(temp2)+'='+str(temp3)
+                var += str(temp1)+'-'+str(temp2)+'='+str(temp3)
             else:
-                temp3=temp2-temp1
-                var+=str(temp2)+'-'+str(temp1)+'='+str(temp3)
+                temp3 = temp2-temp1
+                var += str(temp2)+'-'+str(temp1)+'='+str(temp3)
         var += '\n'
     f.write(var)
     f.close()
 
+
 def Genmid():
-    pass
+    f = open('test1.txt', 'w')
+    var = ''
+    for i in range(50):
+        op = random.randint(1, 4)
+        if op == 1:
+            temp1 = random.randint(1, 50)
+            temp2 = random.randint(1, 50)
+            temp3 = temp1+temp2
+            var += str(temp1)+'+'+str(temp2)+'='+str(temp3)
+        elif op == 2:
+            temp1 = random.randint(1, 100)
+            temp2 = random.randint(1, 100)
+            if temp1 >= temp2:
+                temp3 = temp1-temp2
+                var += str(temp1)+'-'+str(temp2)+'='+str(temp3)
+            else:
+                temp3 = temp2-temp1
+                var += str(temp2)+'-'+str(temp1)+'='+str(temp3)
+        elif op == 3:
+            temp1 = random.randint(1, 10)
+            temp2 = random.randint(1, 10)
+            temp3 = temp1*temp2
+            var+=str(temp1)+'*'+str(temp2)+'='+str(temp3)
+        else:
+            temp1 = random.randint(1, 10)
+            temp2 = random.randint(1, 10)
+            temp3 = temp1*temp2
+            var+=str(temp3)+'/'+str(temp1)+'='+str(temp2)
+        var += '\n'
+    f.write(var)
+    f.close()
 
 
 def Gendif():
@@ -53,3 +84,4 @@ def Genfour(a):
 
 if __name__ == "__main__":
     Genfour(1)
+    Genfour(2)
