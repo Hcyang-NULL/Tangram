5a39faa4a608e0e6c19aea9f9977e62e54ccae3c
HCY <HCY_NULL@163.com>
2019-06-25 22:01:25 +0800

    测试

--- backend/email_test.py
+++ backend/email_test.py
@@ -22,38 +22,28 @@ def log(t):
         f.write(str(t))
 
 @stopit.threading_timeoutable()
-def sendEmail(email):
+def sendEmail(email, username, verify):
     try:
-        print('step 0')
-        smtp = smtplib.SMTP('smtp.163.com',465) 
-        print('step 0.1')
-        print('step 0.2')
+        smtp = smtplib.SMTP_SSL('smtp.163.com',465)  
         smtp.login(c.EMAIL_USER, c.EMAIL_PASSWORD) 
-        print('step 1')
         content = '请点击以下链接完成验证\n'
-        content += c.VERIFY_URL
+        content += c.VERIFY_URL+username+'&verify='+str(verify)
         content += ('\n时间：%s \n')%(str(getTime(False)))
         msg = MIMEText(content,'plain','utf-8')
-        print('step 2')
         subject = '七巧板速算注册'
         msg['Subject'] = Header(subject, 'utf-8')
-        msg['From'] = ('七巧板官方团队 <%s>')%(c.EMAIL_NAME)  
-        # msg['From'] = c.EMAIL_NAME
+        msg['From'] = ('七巧板官方团队 <%s>')%(c.EMAIL_NAME)
         msg['To'] = email
-        print('step 3')
         smtp.sendmail(c.EMAIL_NAME, email, msg.as_string()) 
         smtp.quit()
     except TimeoutException as e:
-        print('error 1')
         log(('发送邮件失败\n目标邮箱:%s\n时间:%s\n')%(email, str(getTime(False))))
         print(e)
         return 1
     except Exception as e:
-        print('error 2')
         log(('发送邮件失败\n目标邮箱:%s\n时间:%s\n')%(email, str(getTime(False))))
         print(e)
         return 1
-    print('step 4')
     log(('发送邮件成功\n目标邮箱:%s\n时间:%s\n')%(email, str(getTime(False))))
     return 0
 
@@ -86,7 +76,7 @@ def temp(email):
     pass
 
 if __name__ == "__main__":
-    res = sendEmail('hcy_null@163.com', timeout = 15)
+    res = sendEmail('hcy_null@163.com', 'hcyang', '', timeout = 15)
     if res == 0:
         print('发送成功')
     elif res == 1:
