ca081b7551a729d16bdd506509af76531d1455b5
HCY <HCY_NULL@163.com>
2019-06-26 20:45:40 +0800

    获取题目ajax成功

--- html/website/exercise/js/exercise.js
+++ html/website/exercise/js/exercise.js
@@ -3,66 +3,60 @@
  * Time: 2019-06-25
  */
 
+let ed = -1;
+let problems = []
+let started = 0
+
 var loguser = ""
 //get parameters in url
 var url = location.search;
 var data = new Object();
-if(url.indexOf("?") != -1)
-{
+if (url.indexOf("?") != -1) {
     var str = url.substr(1);
     var strs = str.split("&");
-    for(var i = 0; i < strs.length; i++)
-    {
+    for (var i = 0; i < strs.length; i++) {
         data[strs[i].split("=")[0]] = (strs[i].split("=")[1]);
     }
 }
-if("loguser" in data)
-{
+if ("loguser" in data) {
     loguser = data["loguser"]
     $('.user-id').children(':first').text(loguser);
 }
 
-$('.home').click(function (e) { 
+$('.home').click(function (e) {
     var tail = "";
-    if(loguser != "")
-    {
-        tail = "?loguser="+loguser;
+    if (loguser != "") {
+        tail = "?loguser=" + loguser;
     }
-    window.location.href = '../../../home.html'+tail
+    window.location.href = '../../../home.html' + tail
 });
 
-$('.login').click(function (e) { 
+$('.login').click(function (e) {
     var tail = "";
-    if(loguser != "")
-    {
-        tail = "?loguser="+loguser;
+    if (loguser != "") {
+        tail = "?loguser=" + loguser;
     }
-    window.location.href = '../../login/html/login.html'+tail
+    window.location.href = '../../login/html/login.html' + tail
 });
 
-$('.signup').click(function (e) { 
+$('.signup').click(function (e) {
     var tail = "";
-    if(loguser != "")
-    {
-        tail = "?loguser="+loguser;
+    if (loguser != "") {
+        tail = "?loguser=" + loguser;
     }
-    window.location.href = '../../signup/html/signup.html'+tail
+    window.location.href = '../../signup/html/signup.html' + tail
 });
 
-$('.rank').click(function (e) { 
+$('.rank').click(function (e) {
     var tail = "";
-    if(loguser != "")
-    {
-        tail = "?loguser="+loguser;
+    if (loguser != "") {
+        tail = "?loguser=" + loguser;
     }
-    window.location.href = '../../rank-list/html/rank-list.html'+tail
+    window.location.href = '../../rank-list/html/rank-list.html' + tail
 });
-
-// Months start at 0 and the timer caluclates to Midnight
-// use month - 1 to get an accurate count down
-let ed = new Date(2019, 8 - 1, 13);
 
 function setup() {
+    // console.log('setup')
     pixelDensity(2.0);
     let canvas = createCanvas(320, 150);
     angleMode(DEGREES);
@@ -70,10 +64,16 @@ function setup() {
 }
 
 function draw() {
+    // console.log('draw')
+    if(started == 0)
+    {
+        return;
+    }
+
     let timer = getTimer();
     clear();
 
-    if ((timer.seconds.text) <=0) {
+    if ((timer.seconds.text) <= 0) {
         let zeroTime = {
             "seconds": {
                 "text": 0,
@@ -123,25 +123,21 @@ function shapeCreator(timer) {
 
 function getTimer() {
     let d = new Date();
+    let time = d.getTime()
+    let distance;
 
-    let distance = ed.getTime() - d.getTime();
+    if (ed == -1) {
+        ed = time;
+        distance = 0;
+    } else {
+        distance = d.getTime() - ed;
+    }
 
-    let days = distance / (1000 * 60 * 60 * 24);
-    Math.floor(days) != 0 ? hmsms = days % Math.floor(days) : hmsms =
-        days; // hmsms - hours, minutes, seconds, milliseconds remaining
-    let daysText = Math.floor(days);
+    if (distance >= 59.9 * 1000) {
+        distance = 60 * 1000;
+    }
 
-    let hours = hmsms * 24;
-    Math.floor(hours) != 0 ? msms = hours % Math.floor(hours) : msms =
-        hours; // msms - minutes, seconds, milliseconds remaining
-    let hoursText = Math.floor(hours);
-
-    let minutes = msms * 60;
-    Math.floor(minutes) != 0 ? sms = minutes % Math.floor(minutes) : sms =
-        minutes; // sms - seconds, milliseconds remaining
-    let minutesText = Math.floor(minutes);
-
-    let seconds = sms * 60;
+    let seconds = distance / 1000;
     Math.floor(seconds) != 0 ? ms = seconds % Math.floor(seconds) : ms = seconds; // ms - milliseconds remaining
     let secondsText = Math.floor(seconds);
 
@@ -160,4 +156,98 @@ function getTimer() {
     };
 
     return finalTime;
+
 }
+
+$(document).ready(function () {
+    $('.progress-rate').css('width', '2%');
+    $('.middle-top').css('display', 'none');
+    $('.middle-bottom').css('display', 'none');
+    $('.buttons').css('display', 'none');
+});
+
+function showMiddle() {
+    $('.middle-top').css('display', 'block');
+    $('.middle-bottom').css('display', 'block');
+    $('.buttons').css('display', 'block');
+}
+
+function handle_problems(data) { 
+    console.log(data)
+ }
+
+$('.easy').click(function (e) { 
+    showMiddle();
+    $('.easy').css('background-color', 'rgb(66, 133, 244)');
+    $('.easy').css('border-radius', '20px');
+    $('.easy').css('color', 'white');
+    $('.medium').css('background-color', 'white');
+    $('.medium').css('color', 'black');
+    $('.hard').css('background-color', 'white');
+    $('.hard').css('color', 'black');
+
+    var data = new Object()
+    data.level = 1;
+    if (problems.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://119.23.248.43/getproblem",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function (response) {
+                handle_problems(response);
+                // started = 1;
+            }
+        });
+    }
+});
+
+$('.medium').click(function (e) { 
+    $('.easy').css('background-color', 'white');
+    $('.easy').css('color', 'black');
+    $('.medium').css('background-color', 'rgb(251, 188, 5)');
+    $('.medium').css('border-radius', '20px');
+    $('.medium').css('color', 'white');
+    $('.hard').css('background-color', 'white');
+    $('.hard').css('color', 'black');
+
+    var data = new Object()
+    data.level = 2;
+    if (problems.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://119.23.248.43/getproblem",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function (response) {
+                console.log(response)
+                started = 1;
+            }
+        });
+    }
+});
+
+$('.hard').click(function (e) { 
+    $('.easy').css('background-color', 'white');
+    $('.easy').css('color', 'black');
+    $('.medium').css('background-color', 'white');
+    $('.medium').css('color', 'black');
+    $('.hard').css('background-color', 'rgb(234, 67, 53)');
+    $('.hard').css('border-radius', '20px');
+    $('.hard').css('color', 'white');
+
+    var data = new Object()
+    data.level = 3;
+    if (problems.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://119.23.248.43/getproblem",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function (response) {
+                console.log(response)
+                started = 1;
+            }
+        });
+    }
+});
