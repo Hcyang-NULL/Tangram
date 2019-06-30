3ce8cd1a6a81435bd5ef4a3982bdbe6b512e68dc
HCY <HCY_NULL@163.com>
2019-06-30 14:07:59 +0800

    规范化位置

--- backend/read.py
+++ backend/read.py
@@ -1,36 +1,30 @@
 # -*- coding: UTF-8 -*-
-import os
 from code import Genfour
 
+'''读取题库中的题目，并且随之更新题库一次'''
 def read(lever):
     if lever == 1:
-        if os.path.isfile('test.txt') == False:
-            Genfour(1)
         f=open("test.txt","r")
         list2=[]
         for line in f.readlines():
             location=str(line).find('=')
-            list1 = [line[0:location],line[location+1:-1]]
+            list1 = [line[0:location+1],line[location+1:-1]]
             list2.append(list1)
         text = list2
     elif lever == 2:
-        if os.path.isfile('test1.txt') == False:
-            Genfour(2)
-        f=open("test1.txt","r")
-        list2=[]
-        for line in f.readlines():
+       f=open("test1.txt","r")
+       list2=[]
+       for line in f.readlines():
             location=str(line).find('=')
-            list1 = [line[0:location],line[location+1:-1]]
+            list1 = [line[0:location+1],line[location+1:-1]]
             list2.append(list1)
-        text = list2
+       text = list2
     elif lever == 3:
-        if os.path.isfile('test2.txt') == False:
-            Genfour(3)
         f=open("test2.txt","r")
         list2=[]
         for line in f.readlines():
             location=str(line).find('=')
-            list1 = [line[0:location],line[location+1:-1]]
+            list1 = [line[0:location+1],line[location+1:-1]]
             list2.append(list1)
         text = list2
     else:
@@ -38,4 +32,11 @@ def read(lever):
     f.close()
     Genfour(lever)
     return text
+
+if __name__ == '__main__':
+    lever=input("输入等级：")
+    lever=int(lever)
+    text=read(lever)
+    print(text)
+    print(text[1])
     
